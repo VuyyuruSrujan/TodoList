883c69be11f8a09a89ce870e739436b5864aa678
srujan <srujan.vuyyuru1@gmail.com>
2025-01-19 09:39:26 +0530

    backend server created and mongo db connected

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors")
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb://127.0.0.1:27017/srujan");
+
+app.listen(5001, () => {
+    console.log("Server is running on port 5001");
+});
